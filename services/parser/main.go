1946404fde95c948caf7c9435f9a3ebcc6c55c5a
Eemeli Saari <saari.eemeli@gmail.com>
2020-01-14 18:56:52 +0200

    Add the document module to parser service

--- services/parser/main.go
+++ services/parser/main.go
@@ -4,45 +4,72 @@ import (
 	"encoding/json"
 	"log"
 
+	"github.com/EemeliSaari/turso/internal/utils"
+
 	"github.com/streadway/amqp"
 
+	"github.com/EemeliSaari/turso/pkg/document"
 	"github.com/EemeliSaari/turso/pkg/rabbit"
 	"github.com/EemeliSaari/turso/pkg/rss"
 )
 
-func handler(delivery amqp.Delivery) {
+type resultHandler struct {
+	*rabbit.Publisher
+}
+
+func (h resultHandler) handler(delivery amqp.Delivery) {
 	var article rss.Article
 
 	err := json.Unmarshal(delivery.Body, &article)
 	if err != nil {
 		log.Print("Failed to deserialize message when parsing.")
 	}
-	log.Printf("Parsed article: %s", article.Title)
+
+	doc, err := document.New([]byte(article.Content))
+	if err != nil {
+		log.Print("Failed to parse document:", article.Title)
+	}
+
+	bin, err := json.Marshal(doc)
+	if err != nil {
+		log.Print("Failed to serialize parsed document.")
+	}
+
+	h.Publish(bin)
 }
 
 func main() {
-	conf := rabbit.ConsumerConfig{
-		Config: &rabbit.Config{
-			Host:         "localhost",
-			Port:         5672,
-			Username:     "dev",
-			Password:     "dev",
-			Vhost:        "/",
-			Queue:        "data",
-			Durable:      false,
-			DeleteUnused: false,
-			Exclusive:    false,
-			NoWait:       false,
-		},
+	base := rabbit.Config{
+		Host:         "localhost",
+		Port:         5672,
+		Username:     "dev",
+		Password:     "dev",
+		Vhost:        "/",
+		Queue:        "data",
+		Durable:      false,
+		DeleteUnused: false,
+		Exclusive:    false,
+		NoWait:       false,
+	}
+	consumerConf := rabbit.ConsumerConfig{
+		Config:  &base,
 		AutoAck: true,
 		NoLocal: false,
 		NoWait:  false,
 		Tag:     "",
 	}
-
-	c, err := rabbit.NewConsumer(conf)
-	if err != nil {
-		panic(err)
+	publisherConf := rabbit.PublisherConfig{
+		Config:    &base,
+		Mandatory: false,
+		Immidiate: false,
 	}
-	c.Consume(handler)
+
+	c, err := rabbit.NewConsumer(consumerConf)
+	utils.FailOnError(err, "failed to start consumer")
+	p, err := rabbit.NewPublisher(publisherConf, "text/plain")
+	utils.FailOnError(err, "failed to start publisher")
+
+	h := resultHandler{Publisher: p}
+
+	c.Consume(h.handler)
 }
